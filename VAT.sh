7691d15bd21278ec0abd32e9a56b6a01d704022d
NaSko-bit <atanas.ivanov@ivanov-group.net>
2026-08-30 12:22:40 +0300

    Added the ability for the user to enter the VAT percentage

--- VAT.sh
+++ VAT.sh
@@ -4,13 +4,17 @@ echo 2 - CHECK VALUE WITHOUT VAT
 read -p "ENTER YOUR CHOICE: " choice
 if [ $choice -eq 1 ]; then
 	read -p "ENTER VALUE WITHOUT VAT: " base
-	vatValue=$(echo "$base * 0.20"|bc)
+	read -p "ENTER VAT %: " vatPercent
+	vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
+	vatValue=$(echo "$base * $vatDecimal"|bc)
 	total=$(echo "$base + $vatValue"|bc)
 	echo THE VALUE OF VAT FOR $base IS $vatValue
 	echo THE TOTAL VALUE IS $total
 elif [ $choice -eq 2 ]; then
 	read -p "ENTER TOTAL VALUE WITH VAT: " total
-	netPrice=$(echo "$total / 1.20"|bc)
+	read -p "ENTER VAT %: " vatPercent
+	vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
+	netPrice=$(echo "$total /(1 + $vatDecimal)"|bc)
 	vatValue=$(echo "$total - $netPrice"|bc)
 	echo THE NET PRICE IS $netPrice
 	echo THE VAT VALUE IS $vatValue

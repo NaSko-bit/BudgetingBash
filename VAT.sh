0ba62115a36b28e3109465f272b11c04ce2e4a62
NaSko-bit <atanas.ivanov@ivanov-group.net>
2026-08-30 12:33:59 +0300

    Made the whole program a simple while loop, so it wont close after each operation. Added white space between operations for more clear User Interface

--- VAT.sh
+++ VAT.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
-echo 1 - CHECK VALUE WITH VAT
-echo 2 - CHECK VALUE WITHOUT VAT
-read -p "ENTER YOUR CHOICE: " choice
-if [ $choice -eq 1 ]; then
-	read -p "ENTER VALUE WITHOUT VAT: " base
-	read -p "ENTER VAT %: " vatPercent
-	vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
-	vatValue=$(echo "$base * $vatDecimal"|bc)
-	total=$(echo "$base + $vatValue"|bc)
-	echo THE VALUE OF VAT FOR $base IS $vatValue
-	echo THE TOTAL VALUE IS $total
-elif [ $choice -eq 2 ]; then
-	read -p "ENTER TOTAL VALUE WITH VAT: " total
-	read -p "ENTER VAT %: " vatPercent
-	vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
-	netPrice=$(echo "$total /(1 + $vatDecimal)"|bc)
-	vatValue=$(echo "$total - $netPrice"|bc)
-	echo THE NET PRICE IS $netPrice
-	echo THE VAT VALUE IS $vatValue
-else
-	echo WRONG INPUT!
-fi
+while [ 1 -eq 1 ]; do
+	echo 1 - CHECK VALUE WITH VAT
+	echo 2 - CHECK VALUE WITHOUT VAT
+	echo 3 - EXIT
+	read -p "ENTER YOUR CHOICE: " choice
+	if [ $choice -eq 1 ]; then
+		read -p "ENTER VALUE WITHOUT VAT: " base
+		read -p "ENTER VAT %: " vatPercent
+		vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
+		vatValue=$(echo "$base * $vatDecimal"|bc)
+		total=$(echo "$base + $vatValue"|bc)
+		echo THE VALUE OF VAT FOR $base IS $vatValue
+		echo THE TOTAL VALUE IS $total
+		echo
+		echo
+		echo
+		echo
+	elif [ $choice -eq 2 ]; then
+		read -p "ENTER TOTAL VALUE WITH VAT: " total
+		read -p "ENTER VAT %: " vatPercent
+		vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
+		netPrice=$(echo "$total /(1 + $vatDecimal)"|bc)
+		vatValue=$(echo "$total - $netPrice"|bc)
+		echo THE NET PRICE IS $netPrice
+		echo THE VAT VALUE IS $vatValue
+		echo
+		echo
+		echo
+		echo
+	elif [ $choice -eq 3 ]; then
+		echo GOODBYE!
+		echo
+		echo
+		echo
+		echo
+		break
+	else
+		echo WRONG INPUT!
+		echo
+		echo
+		echo
+		echo
+	fi
+done

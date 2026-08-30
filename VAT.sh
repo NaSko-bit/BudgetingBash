3c176eb4b5542b3947b34a1fb5de2d70928d0070
NaSko-bit <atanas.ivanov@ivanov-group.net>
2026-08-30 15:11:14 +0300

    Added functions for the two calculations

--- VAT.sh
+++ VAT.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+
+# 1 is base, 2 is vatDecimal
+calculateVAT(){
+	local vatValue=$(echo "$1 * $2"|bc)
+	local total=$(echo "$1 + $vatValue"|bc)
+	echo THE VALUE OF VAT FOR $1 IS $vatValue
+	echo THE TOTAL VALUE IS $total
+	echo
+	echo
+	echo
+	echo
+}
+
+# 1 is total, 2 is vatDecimal
+calculateNet(){
+	local netPrice=$(echo "$1 / (1 + $2)"|bc)
+	local vatValue=$(echo "$1 - $netPrice"|bc)
+	echo THE NET VALUE IS $netPrice
+	echo THE NET VALUE IS $vatValue
+	echo
+	echo
+	echo
+	echo
+}
+
+# MAIN FUNC
 while [ 1 -eq 1 ]; do
 	echo 1 - CHECK VALUE WITH VAT
 	echo 2 - CHECK VALUE WITHOUT VAT
@@ -8,26 +34,12 @@ while [ 1 -eq 1 ]; do
 		read -p "ENTER VALUE WITHOUT VAT: " base
 		read -p "ENTER VAT %: " vatPercent
 		vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
-		vatValue=$(echo "$base * $vatDecimal"|bc)
-		total=$(echo "$base + $vatValue"|bc)
-		echo THE VALUE OF VAT FOR $base IS $vatValue
-		echo THE TOTAL VALUE IS $total
-		echo
-		echo
-		echo
-		echo
+		calculateVAT $base $vatDecimal
 	elif [ $choice -eq 2 ]; then
 		read -p "ENTER TOTAL VALUE WITH VAT: " total
 		read -p "ENTER VAT %: " vatPercent
 		vatDecimal=$(echo "scale=2; $vatPercent / 100"|bc)
-		netPrice=$(echo "$total /(1 + $vatDecimal)"|bc)
-		vatValue=$(echo "$total - $netPrice"|bc)
-		echo THE NET PRICE IS $netPrice
-		echo THE VAT VALUE IS $vatValue
-		echo
-		echo
-		echo
-		echo
+		calculateNet $total $vatDecimal
 	elif [ $choice -eq 3 ]; then
 		echo GOODBYE!
 		echo

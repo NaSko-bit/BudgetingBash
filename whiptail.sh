7efb21070db98af2dd68d81cf4467853d6e46020
NaSko-bit <atanas.ivanov@ivanov-group.net>
2026-08-31 23:54:31 +0300

    Started playing with WHIPTAIL for creating a TUI, builded a simple menu

---
+++ whiptail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#simple menu
+	whiptail --title "GENERIC MENU" --menu "SELECT AN OPTION" 25 78 16 \
+		"Option 1" "It does stuff"\
+		"Option 2" "It does stuff too"\
+		"Option 3" "Try to guess it"

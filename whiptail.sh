8e8f1d5fe362b32be23abc4bfec286116800846e
NaSko-bit <atanas.ivanov@ivanov-group.net>
2026-09-01 14:50:18 +0300

    Added textboxes as redirect from the menu. Will be usefull in the VAT program

--- whiptail.sh
+++ whiptail.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 #simple menu
-	whiptail --title "GENERIC MENU" --menu "SELECT AN OPTION" 25 78 16 \
+	CHOICE=$(whiptail --title "GENERIC MENU" --menu "SELECT AN OPTION" 25 78 16 \
 		"Option 1" "It does stuff"\
 		"Option 2" "It does stuff too"\
-		"Option 3" "Try to guess it"
+		"Option 3" "Try to guess it" 3>&1 1>&2 2>&3)
+
+	case "$CHOICE" in
+		"Option 1")
+			echo "YOU CHOOSED OPTION 1" > option1
+			whiptail --textbox option1 12 80
+			;;
+
+		"Option 2")
+			echo "YOU CHOOSED OPTION 2" > option2
+			whiptail --textbox option2 12 80
+			;;
+		"Option 3")
+			echo "YOU CHOOSED OPTION 3" > option3
+			whiptail --textbox option3 12 80
+			;;
+	esac

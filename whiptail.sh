62ceb5af8d7ad12868aa1a41cfbd451a7dbc1f83
NaSko-bit <atanas.ivanov@ivanov-group.net>
2026-09-02 10:45:59 +0300

    Implented reading the Log File together with scroll text in case the log gets longer than the textbox itself

--- whiptail.sh
+++ whiptail.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+ReadLog(){
+	local log_file=vatLog.txt
+	while read -r line; do
+		echo $line
+	done < $log_file
+}
+
 # main function
 while [ 1 -eq 1 ]; do
-    # Намаляваме размерите на 15 60 4 за сигурност
-    CHOICE=$(whiptail --title "GENERIC MENU" --menu "SELECT AN OPTION" 15 60 4 \
-    "1" "Option 1: It does stuff" \
-    "2" "Option 2: It does stuff too" \
-    "3" "Option 3: Try to guess it" \
-    "4" "EXIT" 3>&1 1>&2 2>&3)
+    CHOICE=$(whiptail --title "GENERIC MENU" --menu "SELECT AN OPTION" 25 78 16 \
+    "1" "VAT LOG" \
+    "2" "FILE LOG" \
+    "3" "EXIT" 3>&1 1>&2 2>&3)
 
-    # Проверка дали потребителят е натиснал Cancel/Esc или whiptail е гръмнал
     if [ $? -ne 0 ]; then
         echo "Whiptail приключи със статус за грешка или изход."
         break
@@ -17,18 +21,14 @@ while [ 1 -eq 1 ]; do
 
     case "$CHOICE" in
         "1")
-            echo "YOU CHOOSED OPTION 1" > option1
-            whiptail --textbox option1 12 50
+            ReadLog > option1
+            whiptail --textbox --scrolltext option1 12 80
             ;;
         "2")
             echo "YOU CHOOSED OPTION 2" > option2
-            whiptail --textbox option2 12 50
+            whiptail --textbox option2 12 80
             ;;
         "3")
-            echo "YOU CHOOSED OPTION 3" > option3
-            whiptail --textbox option3 12 50
-            ;;
-        "4")
             break
             ;;
     esac
